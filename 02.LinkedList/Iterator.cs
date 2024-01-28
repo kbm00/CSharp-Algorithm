7d6fc69c58fda87f964594e9c6667e3b2c75adf4
ehwjr0032@naver.com <ehwjr0032@naver.com>
2024-01-28 20:18:02 +0900

    docs : 연결리스트

---
+++ 02.LinkedList/Iterator.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.LinkedList
+{
+    internal class Iterator
+    {
+        static void Main3()
+        {
+            List<int> list = new List<int>();
+            LinkedList<int> linkedList = new LinkedList<int>();
+            SortedSet<int> set = new SortedSet<int>();
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                list.Add(i);
+                linkedList.AddLast(i);
+                set.Add(i);
+            }
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.Write($"{list[i]} ");
+            }
+
+            /*for(int i=0; i< Linkedlist.Count; i++)
+            {
+                Console.Write($"{linkedList[i] ");
+            }X  */
+
+            for (LinkedListNode<int> node = linkedList.First; node != null; node = node.Next)
+            {
+                Console.Write($"{node.Value} ");
+            }
+
+            foreach (int value in list)
+            {
+                Console.Write($"{value} ");
+            }
+
+            foreach (int value in linkedList)
+            {
+                Console.WriteLine($"{value} ");
+            }
+
+            foreach (int value in set)
+            {
+                Console.WriteLine($"{value} ");
+            }
+            Book book = new Book();
+            foreach (string value in book)
+            {
+                Console.WriteLine($"{value} ");
+            }
+            foreach (int value in Func())
+            {
+                Console.WriteLine($"{value} ");
+            }
+        }
+
+
+        public static IEnumerable<int> Func()
+        {
+            yield return 10;
+            yield return 20;
+            yield return 30;
+            yield return 40;
+            yield return 50;
+        }
+
+
+        public class Book : IEnumerable<string>
+        {
+            public string[] text;
+
+            public IEnumerator<string> GetEnumerator()
+            {
+                throw new NotImplementedException();
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+
+
+
+
+    }
+
+
+}
---
+++ 02.LinkedList/LinkedList.cs
@@ -0,0 +1,210 @@
+﻿using _02.LinkedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Datastructure
+{
+    internal class LinkedList<T>
+    {
+        private LinkedListNode<T> head;
+        private LinkedListNode<T> tail;
+        private int count;
+
+        public LinkedList()
+        {
+            head = null;
+            tail = null;
+            count = 0;
+        }
+
+
+        public LinkedListNode<T> First { get { return head; } }
+        public LinkedListNode<T> Last { get { return tail; } }
+        public int Count { get { return count; } }
+
+
+        public LinkedListNode<T> AddLast(T value)
+        {
+            LinkedListNode<T> newNode = new LinkedListNode<T>(value);
+            if (tail != null)
+            {
+                InsertNodeToEmptyList(newNode);
+            }
+            else
+            {
+                InsertNodeAfter(tail, newNode);
+            }
+            return newNode;
+        }
+
+
+        public LinkedListNode<T> AddFirst(T value)
+        {
+            LinkedListNode<T> newNode = new LinkedListNode<T>(value);
+            if (count == 0)
+            {
+                InsertNodeToEmptyList(newNode);
+            }
+            else
+            {
+                InsertNodeBefore(head, newNode);
+            }
+
+            return newNode;
+        }
+
+
+        public LinkedListNode<T> AddBefore(LinkedListNode<T> node, T value)
+        {
+            LinkedListNode<T> newNode = new LinkedListNode<T>(value);
+            InsertNodeBefore(node, newNode);
+            return newNode;
+        }
+
+        public LinkedListNode<T> Find(T value)
+        {
+
+            return null;
+        }
+
+
+        private void InsertNodeBefore(LinkedListNode<T> node, LinkedListNode<T> newNode)
+        {
+            LinkedListNode<T> prevNode = node.prev;
+
+            // 1. newNode의 prev를 node의 prev로
+            newNode.prev = prevNode;
+
+            // 2. newNode의 next를 node로
+            newNode.next = node;
+
+            // 3. node 앞의 노드의 유무
+
+            if (node == head)  // 3-1. head를 newNode로
+            {
+                head = newNode;
+            }
+            else    // 3-2. node의 prev의 next를 newNode로
+            {
+                prevNode.next = newNode;
+            }
+
+            // 4 node의 prev를 next로
+            node.prev = newNode;
+
+            count++;
+
+        }
+
+        public void InsertNodeAfter(LinkedListNode<T> node, LinkedListNode<T> newNode)
+        {
+            newNode.prev = node;
+            newNode.next = node.Next;
+
+            if (node == tail)
+            {
+                tail = newNode;
+            }
+            else
+            {
+                node.next.prev = newNode;
+            }
+
+            node.next = newNode;
+
+            count++;
+
+
+        }
+
+        private void InsertNodeToEmptyList(LinkedListNode<T> newNode)
+        {
+            if (count != 0)
+                throw new InvalidOperationException();
+
+            head = newNode;
+            tail = newNode;
+            count++;
+        }
+        public void Remove(T value)
+        {
+
+        }
+
+
+
+
+        public void Remove(LinkedListNode<T> node)
+        {
+            RemoveNode(node);
+        }
+
+        public void RemoveFirtst()
+        {
+            RemoveNode(head);
+        }
+
+        public void RemoveLast()
+        {
+            RemoveNode(tail);
+        }
+
+
+
+
+        private void RemoveNode(LinkedListNode<T> node)
+        {
+            if (node == null)
+                throw new ArgumentException("node");
+
+            if (head == node)
+                head = node.next;
+            if (tail == node)
+                tail = node.prev;
+
+            if (node.prev != null)
+                node.prev.next = node.next;
+            if (node.next != null)
+                node.next.prev = node.prev;
+
+            count--;
+        }
+
+
+
+    }
+
+    public class LinkedListNode<T>
+    {
+        private T value;
+
+        public LinkedListNode<T> prev;
+        public LinkedListNode<T> next;
+
+        public LinkedListNode(T value)
+        {
+            this.value = value;
+            this.prev = null;
+            this.next = null;
+
+        }
+
+        public LinkedListNode(T value, LinkedListNode<T> prev, LinkedListNode<T> next)
+        {
+            this.value = value;
+            this.prev = prev;
+            this.next = next;
+        }
+
+        public LinkedListNode<T> Prev { get { return prev; } }
+        public LinkedListNode<T> Next { get { return next; } }
+
+        public T Value { get { return value; } }
+    }
+
+
+}
